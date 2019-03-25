81ccb0b1c156e784c84c0b8e147d590b027cea21
Giap Tran <txgvnn@gmail.com>
2019-03-25 22:26:30 +0700

    Update documentation

--- gnus-summary-repo.el
+++ gnus-summary-repo.el
@@ -11,6 +11,12 @@
 ;; You have to be at Summary mode (defined in ‘gnus-sum.el’) before
 ;; call the gnus-summary-repo-* function
 
+;;; Best practice
+;; 1. Create a new group IMAP for only store your notes.
+;; Example: nnimap+Gmail:Notes
+;; 2. Go to your group with all of the articles (unlimited articles, you will be Summary mode)
+;; 3. Use gnus-summary-repo-* function
+
 ;;; Code:
 
 (require 'gnus-sum)
